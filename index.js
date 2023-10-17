f1553ae39398f0fe310c2b015bdcc07190100856
Akash Nair <akashnair278@gmail.com>
2023-10-17 15:18:31 +0530

    Added feature of sounds from keyboard in js

--- index.js
+++ index.js
@@ -6,8 +6,19 @@ for(var i = 0; i<document.querySelectorAll("button").length; i++){
 document.querySelectorAll("button")[i].addEventListener("click", function(){
 
     var buttonInnerHTML = this.innerHTML;
+    makeSound(buttonInnerHTML);
+    
+});
+}
 
-    switch (buttonInnerHTML) {
+document.addEventListener("keypress", function(event){
+    makeSound(event.key);
+
+
+});
+
+function makeSound(key){
+    switch (key) {
         case "w":
             var crash = new Audio('/sounds/crash.mp3');
             crash.play();
@@ -19,30 +30,26 @@ document.querySelectorAll("button")[i].addEventListener("click", function(){
         case "s":
             var snare = new Audio('/sounds/snare.mp3');
             snare.play();
+            break;
         case "d":
             var tom1 = new Audio('/sounds/tom-1.mp3');
             tom1.play();
+            break;
         case "j":
             var tom2 = new Audio('/sounds/tom-2.mp3');
             tom2.play();
+            break;
         case "k":
             var tom3 = new Audio('/sounds/tom-3.mp3');
             tom3.play();
+            break;
         case "l":
             var tom4 = new Audio('/sounds/tom-4.mp3');
             tom4.play();
+            break;
         default:
+            break;
             
     }
-  
-
-
-
-})
-
 }
-
-
-
-
 
